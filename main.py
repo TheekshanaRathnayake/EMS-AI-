fdcdda95fdb2df2913f715a767ca6fc6fd99635c
Theekshana Rathnayake <119834597+TheekshanaRathnayake@users.noreply.github.com>
2025-04-10 11:55:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ VIDEOS_DICT = {
 
 #Model Configurations
 MODEL_DIR = ROOT/'weights'
-DETECTION_MODEL = MODEL_DIR/'best.pt'
+DETECTION_MODEL = MODEL_DIR/'best(7).pt'
 
 #In case of your custom model
 #DETECTION_MODEL = MODEL_DIR/'custom_model_weight.pt'
